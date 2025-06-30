c34db8a12400248b6eeeb34323d89308a7180e70
weeevo <will@wennik.com>
2025-06-29 23:46:39 -0400

    search mostly functional!

--- app.js
+++ app.js
@@ -4,43 +4,71 @@ const path = require('path');
 const port = 3000
 
 app.use(express.static(path.join(__dirname, 'assets')));
+app.use(express.json());
+
 app.set('views', './pages')
 app.set('view engine', 'ejs');
 
+
 app.get('/', (req, res) => {
-  res.render('index.ejs')
+    res.render('index.ejs')
 })
 
-app.post('/searchSongs', (req, res) => {
-    let songName = req.body.songName;
-    songName = songName.replace(/(<([^>]+)>)/ig, "");
-    let endpoint = `https://api.spotify.com/v1/search?q=${songName}&type=track&market=EN&limit=10&offset=0`
+app.post('/search', (req, res) => {
+    let { searchTerm } = req.body;
 
-    search()
+    // get list of songs for the search term
+    search();
     async function search() {
-        try {
-          const response = await fetch(endpoint, {
-              method: "GET",
-              headers: {
-                  "Content-Type": "application/json",
-                  "Authorization": `Bearer ${config.OpenAI_API_Key}`
-              },
-              body: JSON.stringify({
-                  model: "gpt-4o-mini",
-                  messages: [{ role: "user", content: fullprompt }],
-                  max_tokens: 50
-              })
-          });
+        let songsList = (await fetchWebApi(searchTerm)).tracks.items;
+    
+        // just get song name and artist for the resulting songs
+        const songsNameArtistList = songsList.map((song) => [song.album.images[(song.album.images).length - 1].url, song.name, song.artists[0].name])
+        // console.log(songsNameArtistList);
+        res.status(200).json({ status: "success", results: songsNameArtistList });
+    }
+})
 
-            res.status(200).json({ status: "report success" });
-        } catch (error) {
-            console.error("Error saving to Google Sheets:", error);
-            res.status(500).json({ status: "google sheets error", error: error.message });
-        }
+async function fetchWebApi(searchTerm) {
+    const CLIENT_SECRET = "0f1fa37afded4805aecdd12f9b507e90";
+    const CLIENT_ID = "a8fbe0e6900049039ec4554f9a51dba9";
+
+    // get access token
+    const tokenRes = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": "Basic " + Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString("base64")
+        },
+        body: "grant_type=client_credentials"
+    })
+
+    let token = (await tokenRes.json()).access_token;
+
+    // get songs with search term
+    const query = new URLSearchParams({
+        q: searchTerm,
+        type: 'track',
+        limit: 10,
+        offset: 0,
+        market: 'US'
+    }).toString();
+
+    const songsRes = await fetch(`https://api.spotify.com/v1/search?${query}`, {
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${token}`
+        },
+    });
+
+    if (!songsRes.ok) {
+        throw new Error(`Spotify API error ${res.status}`);
     }
 
-})
+    let songsList = await songsRes.json();
+    return songsList;
+}
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
+    console.log(`App listening on port ${port}`)
 })
--- assets/search.js
+++ assets/search.js
@@ -1,30 +1,84 @@
-const serverURL = "localhost:3000";
-const searchInput = document.getElementById("search-input");
-const searchBtn = document.getElementById("search-btn");
-
+const serverURL = "http://localhost:3000";
 let timer = setTimeout(() => {}, 1);
+
+
+const searchInput = document.getElementById("search-input");
+const suggestions = document.getElementById("suggestions");
 
 searchInput.addEventListener("keyup", checkTimeout);
 
+// click outside search box or results box to close results
+window.addEventListener("click", (e) => {
+    if(e.target != suggestions && e.target != searchInput && !suggestions.contains(e.target)){
+        suggestions.classList.remove("suggestions-visible");
+    }
+    else if(e.target == searchInput && (searchInput.value != "")){
+        setTimeout(() => {
+            suggestions.classList.add("suggestions-visible");
+        }, 300);
+    }
+})
+
+// check if the user pauses typing before searching for matching songs
 function checkTimeout() {
     clearTimeout(timer);
     timer = setTimeout(() => {
-        searchSongs();
+        if(searchInput.value) {
+            searchSongs();
+        }
     }, 300)
 }
 
- function searchSongs(){
+// get the song that the user searched for
+async function searchSongs(){
     var searchTerm = searchInput.value
     console.log(searchTerm);
 
-    const res = await fetch(serverURL + '/searchSongs',
+    const res = await fetch(serverURL + '/search',
     {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            songName: searchTerm
+            searchTerm: searchTerm
         })
     })
+
+    const songs = await res.json();
+    if(songs.status == "success"){
+        removeSuggestions();
+        suggestions.classList.add("suggestions-visible");
+        songs.results.forEach((song) => {
+            const button = document.createElement("button");
+            button.classList.add("song-inline")
+
+            const albCover = document.createElement("img");
+            albCover.src = song[0];
+            albCover.alt = "";
+
+            const songName = document.createElement("p");
+            songName.textContent = song[1];
+
+            const artistName = document.createElement("span");
+            artistName.textContent = song[2];
+
+            button.appendChild(albCover);
+            button.appendChild(songName);
+            button.appendChild(artistName);
+            suggestions.appendChild(button);
+        });
+    }
+}
+
+// clear previous results before adding new ones
+function removeSuggestions(){
+    let searchResults = document.querySelectorAll(".song-inline");
+    console.log(searchResults);
+    if(searchResults){
+        for(let i = 0; i < searchResults.length; i++){
+            let child = searchResults[i]
+            suggestions.removeChild(child)
+        }
+    }
 }
