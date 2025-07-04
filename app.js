49ad49ae4442215d87229def77eb442ad51d2b96
weeevo <will@wennik.com>
2025-07-04 16:51:50 -0400

    updates

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const app = express()
 const path = require('path');
 const port = 3000
 
+require('dotenv').config();
+
 app.use(express.static(path.join(__dirname, 'assets')));
 app.use(express.json());
 
 app.set('views', './pages')
 app.set('view engine', 'ejs');
 
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+})
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
@@ -16,22 +21,71 @@ app.get('/', (req, res) => {
 
 app.post('/search', (req, res) => {
     let { searchTerm } = req.body;
+    getSuggestions();
 
-    // get list of songs for the search term
-    search();
-    async function search() {
-        let songsList = (await fetchWebApi(searchTerm)).tracks.items;
-    
-        // just get song name and artist for the resulting songs
-        const songsNameArtistList = songsList.map((song) => [song.album.images[(song.album.images).length - 1].url, song.name, song.artists[0].name])
-        // console.log(songsNameArtistList);
-        res.status(200).json({ status: "success", results: songsNameArtistList });
+    // get and send list of songs for the search term
+    async function getSuggestions() {
+        let results = (await fetchSpotifyApi(searchTerm)).tracks.items;
+        let suggestions = deDuplicate(results, 10)
+        res.status(200).json({ status: "success", results: suggestions });
     }
 })
 
-async function fetchWebApi(searchTerm) {
-    const CLIENT_SECRET = "0f1fa37afded4805aecdd12f9b507e90";
-    const CLIENT_ID = "a8fbe0e6900049039ec4554f9a51dba9";
+app.post('/searchSimilar', (req, res) => {
+    var { title, artist } = req.body;
+    getSimilar();
+
+    // get and send similar songs for input song
+    async function getSimilar() {
+        let songId = (await fetchSpotifyApi(title, artist)).tracks.items[0].id;
+        let results = (await findSimilarSongs(songId));
+        console.log("results", results);
+    }
+})
+
+async function fetchSpotifyApi(title, artist = "") {
+    let token = await getToken();
+    let query;
+
+    if (artist != "") {
+        // get the actual song after we've searched for it with title + artist
+        query = new URLSearchParams({
+            q: title, artist,
+            type: 'track',
+            limit: 1,
+            offset: 0,
+            market: 'US'
+        }).toString();
+    }
+    else {
+        // get suggested results from just searching the title
+        query = new URLSearchParams({
+            q: title,
+            type: 'track',
+            limit: 50,
+            offset: 0,
+            market: 'US'
+        }).toString();
+    }
+
+    const songsRes = await fetch(`https://api.spotify.com/v1/search?${query}`, {
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${token}`
+        },
+    });
+
+    if (!songsRes.ok) {
+        throw new Error(`Spotify API error ${songsRes.status}`);
+    }
+
+    let songsList = await songsRes.json();
+    return songsList;
+}
+
+async function getToken() {
+    const CLIENT_SECRET = process.env.CLIENT_SECRET;
+    const CLIENT_ID = process.env.CLIENT_ID;
 
     // get access token
     const tokenRes = await fetch("https://accounts.spotify.com/api/token", {
@@ -44,31 +98,44 @@ async function fetchWebApi(searchTerm) {
     })
 
     let token = (await tokenRes.json()).access_token;
-
-    // get songs with search term
-    const query = new URLSearchParams({
-        q: searchTerm,
-        type: 'track',
-        limit: 10,
-        offset: 0,
-        market: 'US'
-    }).toString();
-
-    const songsRes = await fetch(`https://api.spotify.com/v1/search?${query}`, {
-        method: "GET",
-        headers: {
-            "Authorization": `Bearer ${token}`
-        },
-    });
-
-    if (!songsRes.ok) {
-        throw new Error(`Spotify API error ${res.status}`);
-    }
-
-    let songsList = await songsRes.json();
-    return songsList;
+    return token
 }
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
-})
+function deDuplicate(songsList, maxLength) {
+    let seen = new Set();
+    const uniqueTracks = [];
+
+    // filter for 10 unique results
+    for (const track of songsList) {
+        const artist = track.artists[0]?.name?.toLowerCase().trim();
+        const name = track.name.toLowerCase().replace(/\*/g, '').trim();
+        const key = `${artist}-${name}`;
+
+        if (!seen.has(key)) {
+            seen.add(key);
+            uniqueTracks.push(track);
+        }
+
+        if (uniqueTracks.length === maxLength) break;
+    }
+
+    // format for frontend
+    let artistList = [];
+    let suggestions = [];
+
+    for (let i = 0; i < uniqueTracks.length; i++) {
+        let artists = ""
+
+        for (let j = 0; j < uniqueTracks[i].artists.length; j++) {
+            artists += uniqueTracks[i].artists[j].name;
+            if (j != uniqueTracks[i].artists.length - 1) {
+                artists += ", "
+            }
+        }
+
+        artistList[i] = artists;
+        suggestions[i] = [uniqueTracks[i].album.images[(uniqueTracks[i].album.images).length - 1].url, uniqueTracks[i].name, artistList[i]]
+    }
+
+    return suggestions;
+}
--- assets/search.js
+++ assets/search.js
@@ -1,20 +1,20 @@
-const serverURL = "http://localhost:3000";
+const serverURL = "http://localhost:3000"
 let timer = setTimeout(() => {}, 1);
-
 
 const searchInput = document.getElementById("search-input");
 const suggestions = document.getElementById("suggestions");
+const similarSongs = document.querySelector(".similar-songs");
 
 searchInput.addEventListener("keyup", checkTimeout);
 
 // click outside search box or results box to close results
 window.addEventListener("click", (e) => {
     if(e.target != suggestions && e.target != searchInput && !suggestions.contains(e.target)){
-        suggestions.classList.remove("suggestions-visible");
+        suggestions.classList.add("hidden");
     }
     else if(e.target == searchInput && (searchInput.value != "")){
         setTimeout(() => {
-            suggestions.classList.add("suggestions-visible");
+            suggestions.classList.remove("hidden");
         }, 300);
     }
 })
@@ -32,7 +32,6 @@ function checkTimeout() {
 // get the song that the user searched for
 async function searchSongs(){
     var searchTerm = searchInput.value
-    console.log(searchTerm);
 
     const res = await fetch(serverURL + '/search',
     {
@@ -45,22 +44,25 @@ async function searchSongs(){
         })
     })
 
-    const songs = await res.json();
+    let songs = await res.json();
     if(songs.status == "success"){
         removeSuggestions();
-        suggestions.classList.add("suggestions-visible");
+        suggestions.classList.remove("hidden");
         songs.results.forEach((song) => {
             const button = document.createElement("button");
             button.classList.add("song-inline")
+            button.onclick = searchSong;
 
             const albCover = document.createElement("img");
             albCover.src = song[0];
             albCover.alt = "";
 
-            const songName = document.createElement("p");
+            const songName = document.createElement("p")
+            songName.id = "songName";
             songName.textContent = song[1];
 
             const artistName = document.createElement("span");
+            artistName.id = "artistName"
             artistName.textContent = song[2];
 
             button.appendChild(albCover);
@@ -80,5 +82,61 @@ function removeSuggestions(){
             let child = searchResults[i]
             suggestions.removeChild(child)
         }
+    }
+}
+
+// searching for songs
+function searchSong(){
+    let title = this.childNodes[1].textContent;
+    let artist = this.childNodes[2].textContent;
+
+    suggestions.classList.add("hidden");
+    similarSongs.classList.remove("hidden");
+    document.getElementById("echo-name").textContent = title;
+    document.getElementById("echo-artist").textContent = artist;
+
+    searchSimilarSongs(title, artist);
+}
+
+async function searchSimilarSongs(title, artist){
+    const res = await fetch(serverURL + '/searchSimilar',
+    {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            title: title,
+            artist: artist
+        })
+    })
+
+    const songs = await res.json();
+    if(songs.status == "success"){
+        console.log(songs)
+        // removeSuggestions();
+        // suggestions.classList.remove("hidden");
+        // songs.results.forEach((song) => {
+        //     const button = document.createElement("button");
+        //     button.classList.add("song-inline")
+        //     button.onclick = searchSong;
+
+        //     const albCover = document.createElement("img");
+        //     albCover.src = song[0];
+        //     albCover.alt = "";
+
+        //     const songName = document.createElement("p")
+        //     songName.id = "songName";
+        //     songName.textContent = song[1];
+
+        //     const artistName = document.createElement("span");
+        //     artistName.id = "artistName"
+        //     artistName.textContent = song[2];
+
+        //     button.appendChild(albCover);
+        //     button.appendChild(songName);
+        //     button.appendChild(artistName);
+        //     suggestions.appendChild(button);
+        // });
     }
 }
